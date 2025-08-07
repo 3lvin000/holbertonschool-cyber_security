d6931886170659cd9c7d2e68cb2ab9413044ca20
3lvin000 <elvinmurselov8@gmail.com>
2025-08-07 18:07:33 -0400

    Add script to monitor processes of a user excluding VSZ and RSS = 0

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,cmd | grep -vE '^\s*USER|[[:space:]]0[[:space:]]0$'
+ps aux | grep -E "^$1[[:space:]]" | grep -v 'VSZ' | grep -vE '[[:space:]]0[[:space:]]0[[:space:]]'
