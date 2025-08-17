bbdbe632d18a229e3a779eb54467e333eb304924
3lvin000 <elvinmurselov8@gmail.com>
2025-08-17 17:43:39 -0400

    Add script to extract whois info inti CSV

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 k#!/bin/bash
-[ -z "$1" ] && { >&2 printf "Usage: %s <domain>\n" "$0"; exit 1; }; whois "$1" | awk -F': ' '/Registrant Organization|Admin Organization|Tech Organization|Registrant [^O]|Admin [^O]|Tech [^O]/{gsub(/^[ \t]+|[ \t]+$/, "", $2); if($2 != "") print $1, $2}' > "$1.csv"
+[ -z "$1" ] && { >&2 printf "Usage: %s <domain>\n" "$0"; exit 1; }; whois "$1" | awk -F': ' 'BEGIN{OFS=","} /Registrant (Organization|State\/Province|Country|Email)|Admin (Organization|State\/Province|Country|Email)|Tech (Organization|State\/Province|Country|Email)/ {gsub(/^[ \t]+|[ \t]+$/, "", $2); if($2 != "") print $1, $2}' > "$1.csv"
