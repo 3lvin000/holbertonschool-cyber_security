0c3e829ebf4930d4ba561da02a7de66978d564bd
kali <kali@kali.kali>
2025-09-16 03:31:09 -0400

    .

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-curl -X -s -H "Host: $1" -d "$3" "$2"
+
+NEW_HOST="$1"
+TARGET_URL="$2"
+FORM_DATA="$3"
+
+curl -s -X POST "$TARGET_URL" \
+  -H "Host: $NEW_HOST" \
+  -d "$FORM_DATA"
+
