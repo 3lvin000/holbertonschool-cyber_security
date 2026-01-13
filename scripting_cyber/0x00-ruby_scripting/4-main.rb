0fc02de02b213d4341bfbdc1c9a675d2968e5831
3lvin000 <elvinmurselov8@gmail.com>
2026-01-13 14:15:43 -0500

    ready

---
+++ scripting_cyber/0x00-ruby_scripting/4-main.rb
@@ -0,0 +1,13 @@
+requirerelative ‘3-readfile’
+
+requirerelative ‘4-writefile’
+
+file1_path = ‘file2.json’
+
+file2_path = ‘file.json’
+
+mergejsonfiles(file1path, file2path)
+
+puts ‘File merged successfully!’
+
+countuserids(file2_path)
